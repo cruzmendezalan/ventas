0579f86c60ae95205a45d1220cc9a5e22c51be73
Alan Giovani Cruz Mendez <cruzmendezalan@gmail.com>
2016-03-01 17:00:13 -0600

    Vista vender

--- ventas/resources/views/empleado/index.blade.php
+++ ventas/resources/views/empleado/index.blade.php
@@ -7,15 +7,29 @@
 @section('main')
 <div class="container">
 	<div class="row">
-		<div class="col-md-3">
+		<div class="col-md-4">
 			<div class="panel panel-primary">
 				<div class="panel-heading text-center">
 					<div class="panel-title">
 						<h4 class="txt-white">Lista de productos</h3>
-						<div class="inner-addon right-addon">
-						    <i class="glyphicon glyphicon-search txt-gris"></i>
-						    <input type="text" class="form-control" placeholder="Buscar producto" />
-						</div>
+						<table>
+							<tr>
+								<td>
+									<select name="" id="" class="form-control">
+										<option value="a">Todos </option>
+										<option value="a">Categoria A</option>
+										<option value="a">Categoria B</option>
+										<option value="a">Categoria C</option>
+									</select>
+								</td>
+								<td>
+									<div class="inner-addon right-addon">
+									    <i class="glyphicon glyphicon-search txt-gris"></i>
+									    <input type="text" class="form-control" placeholder="Buscar producto" />
+									</div>
+								</td>
+							</tr>
+						</table>
 					</div>
 				</div>
 				<div class="panel-body fixed-panel">
@@ -36,7 +50,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-9">
+		<div class="col-md-8">
 			<div class="panel panel-primary">
 				<div class="panel-heading">
 					<div class="panel-title text-center">
@@ -52,7 +66,7 @@
 									</td>
 									<td class="col-md-6">
 										<select name="" id="" class="form-control">
-											<option value="a">No Cliente</option>
+											<option value="a">Sin Cliente</option>
 											<option value="a">Cliente A</option>
 											<option value="a">Cliente B</option>
 											<option value="a">Cliente C</option>
@@ -73,72 +87,86 @@
 				<div class="panel-body fixed-panel">
 					<table class="table table-bordered table-hover table-striped">
 						<thead class="text-center">
-							<th>Cantidad</th>
-							<th>Producto</th>
-							<th>P/U</th>
-							<th>Subtotal</th>
+							<th class="col-md-1 text-center">Cantidad</th>
+							<th class="col-md-6 text-center">Descripción</th>
+							<th class="col-md-2 text-center">P/U</th>
+							<th class="col-md-3 text-center">Subtotal</th>
 						</thead>
+						<tbody>
+							<tr>
+								<td>5</td>
+								<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus tempora est ullam maiores eaque omnis temporibus perspiciatis totam a illo, dolorum nostrum, laudantium praesentium voluptas velit sapiente, quasi veniam esse.</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+							<tr>
+								<td>5</td>
+								<td>...</td>
+								<td>MXN 45.50</td>
+								<td>MXN 300.00</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="panel-footer">
+					<table class="table table-bordered">
 						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
+							<td class="col-md-6"></td><td class="col-md-4">Subtotal</td><td class="col-md-2"></td>
 						</tr>
 						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
+							<td class="col-md-6"></td><td class="col-md-4">IVA</td><td class="col-md-2"></td>
 						</tr>
 						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
+							<td class="col-md-6"></td><td class="col-md-4">Total</td><td class="col-md-2"></td>
 						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-						<tr>
-							<td>5</td>
-							<td>...</td>
-							<td>MXN 45.50</td>
-							<td>MXN 300.00</td>
-						</tr>
-
 					</table>
 				</div>
 			</div>
